e358d50914883498b885b69cbe66ce8d319fa48a
0xSJ <jainsanmati846@gmail.com>
2024-12-07 16:22:00 +0530

    add server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+import config from './config.js'
+import productRoute from './routes/productRoute.js'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+//routes
+app.use('/api', productRoute)
+
+app.listen(config.port, () => console.log(`Server is live @ ${config.hostUrl}`))
