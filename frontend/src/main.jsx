c55d9b07fcd98506dadfe67388b40127cc76bd89
0xSJ <jainsanmati846@gmail.com>
2024-12-25 13:40:22 +0530

    style: enhance CookieConsent component styling and adjust scroll acceptance percentage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,9 +17,27 @@ createRoot(document.getElementById('root')).render(
     <Toaster />
     <CookieConsent
       buttonText='Ok'
-      style={{ background: '#2B373B' }}
+      style={{
+        background: '#2B373B',
+        fontSize: '12px',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+      buttonStyle={{
+        color: '#4e503b',
+        fontSize: '10px',
+        background: '#f1d600',
+        borderRadius: '5px',
+        padding: '8px 15px',
+        margin: '15px',
+        border: 'none',
+        cursor: 'pointer',
+      }}
+      contentStyle={{
+        maxWidth: '72%',
+      }}
       acceptOnScroll
-      acceptOnScrollPercentage={15}>
+      acceptOnScrollPercentage={5}>
       ProcratiNix uses cookies to enhance your experience. By continuing to visit this site you
       agree to our use of cookies.
     </CookieConsent>
